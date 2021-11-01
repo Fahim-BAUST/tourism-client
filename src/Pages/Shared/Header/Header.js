41a6608ccaba7e895c52f721446af539dea64110
Fahim-BAUST <ashhabf50@gmail.com>
2021-11-01 15:08:55 +0600

    test

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -78,7 +78,7 @@ const Header = () => {
 
                                             }}
                                         >
-                                              My orders
+                                              My order
                                         </NavLink>
 
 
