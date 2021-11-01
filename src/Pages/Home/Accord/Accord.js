a34d4bdb7a22265ed18b6e4a1eeba292fd77186b
Fahim-BAUST <ashhabf50@gmail.com>
2021-11-01 19:45:12 +0600

    change my order page

--- src/Pages/Home/Accord/Accord.js
+++ src/Pages/Home/Accord/Accord.js
@@ -1,72 +1,147 @@
-import React from 'react';
+import React from "react";
 
 const Accord = () => {
-    return (
-        <div>
-
-            <div className="container pt-5 pb-4" style={{ marginTop: "100px" }}>
-                <div className="row ">
-                    <div className="col-lg-6 col-xs-12 col-sm-12 col-md-6 col-xl-6 pe-5">
-                        <h3 style={{ color: "#333" }}>We are happy to make you happier</h3>
-                        <img className="img-fluid mt-4" src="http://kodeforest.com/wp-demo/travel/wide/wp-content/uploads/2016/01/Untitled-1.jpg" alt="" />
-
-
+  return (
+    <div>
+      <div className="container pt-5 pb-4" style={{ marginTop: "100px" }}>
+        <div className="row ">
+          <div className="col-lg-6 col-xs-12 col-sm-12 col-md-6 col-xl-6 ">
+            <h3
+              style={{
+                color: "#333",
+                borderLeft: "5px solid #c29d59",
+                borderRadius: "5px",
+              }}
+            >
+              We are happy to make you happier
+            </h3>
+            <img
+              className="img-fluid mt-4 accord-image"
+              src="http://kodeforest.com/wp-demo/travel/wide/wp-content/uploads/2016/01/Untitled-1.jpg"
+              alt=""
+            />
+          </div>
+          <div className="col-lg-6 col-xs-12 col-sm-12 col-md-6 col-xl-6 ps-3 acoord-margin">
+            <h2>Need Help ?</h2>
+            <h5>Don’t Worry We are here for you.</h5>
+            <div className="mb-5 bg-light mt-4">
+              <div
+                className="accordion accordion-flush"
+                id="accordionFlushExample"
+              >
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="flush-headingOne">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapseOne"
+                      aria-expanded="false"
+                      aria-controls="flush-collapseOne"
+                    >
+                      Are there any extra fees that I will have to pay?
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseOne"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="flush-headingOne"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                      No, we do not charge any extra fees or fuel surcharges.
+                      The listed price is the price you pay. Including tax.
                     </div>
-                    <div className="col-lg-6 col-xs-12 col-sm-12 col-md-6 col-xl-6 ps-3">
-                        <h2>Need Help ?</h2>
-                        <h5>Don’t Worry We are here for you.</h5>
-                        <div className="mb-5 bg-light mt-4">
-                            <div className="accordion accordion-flush" id="accordionFlushExample">
-                                <div className="accordion-item">
-                                    <h2 className="accordion-header" id="flush-headingOne">
-                                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                            Are there any extra fees that I will have to pay?
-                                        </button>
-                                    </h2>
-                                    <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                        <div className="accordion-body">No, we do not charge any extra fees or fuel surcharges. The listed price is the price you pay. Including tax.</div>
-                                    </div>
-                                </div>
-                                <div className="accordion-item">
-                                    <h2 className="accordion-header" id="flush-headingTwo">
-                                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                                            Should I print a receipt to show the tour guide?
-                                        </button>
-                                    </h2>
-                                    <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                                        <div className="accordion-body">We understand that you may not have a printer handy when you’re traveling so it is not necessary to have a printed copy. However, we require that you show ID matching your reservation and also the Order # that is immediately emailed to you after you make your reservation.</div>
-                                    </div>
-                                </div>
-                                <div className="accordion-item">
-                                    <h2 className="accordion-header" id="flush-headingThree">
-                                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                            How much does it cost to do a private tour?
-                                        </button>
-                                    </h2>
-                                    <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                                        <div className="accordion-body">Please contact us to get the latest information on your desired trip</div>
-                                    </div>
-                                </div>
-                                <div className="accordion-item">
-                                    <h2 className="accordion-header" id="flush-headingFour">
-                                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
-                                            What should I wear?
-                                        </button>
-                                    </h2>
-                                    <div id="flush-collapseFour" className="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
-                                        <div className="accordion-body">Wear whatever is comfortable. It is recommended to have a sturdy pair of broken-in trail shoes, boots, or sneakers. It is preferable to dress in layers and wear clothing that will wick away perspiration and keep you dry and comfortable</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
+                  </div>
                 </div>
-
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="flush-headingTwo">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapseTwo"
+                      aria-expanded="false"
+                      aria-controls="flush-collapseTwo"
+                    >
+                      Should I print a receipt to show the tour guide?
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseTwo"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="flush-headingTwo"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                      We understand that you may not have a printer handy when
+                      you’re traveling so it is not necessary to have a printed
+                      copy. However, we require that you show ID matching your
+                      reservation and also the Order # that is immediately
+                      emailed to you after you make your reservation.
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="flush-headingThree">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapseThree"
+                      aria-expanded="false"
+                      aria-controls="flush-collapseThree"
+                    >
+                      How much does it cost to do a private tour?
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseThree"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="flush-headingThree"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                      Please contact us to get the latest information on your
+                      desired trip
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="flush-headingFour">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapseFour"
+                      aria-expanded="false"
+                      aria-controls="flush-collapseFour"
+                    >
+                      What should I wear?
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseFour"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="flush-headingFour"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                      Wear whatever is comfortable. It is recommended to have a
+                      sturdy pair of broken-in trail shoes, boots, or sneakers.
+                      It is preferable to dress in layers and wear clothing that
+                      will wick away perspiration and keep you dry and
+                      comfortable
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Accord;
--- src/Pages/Order/MyOrder.js
+++ src/Pages/Order/MyOrder.js
@@ -1,61 +1,113 @@
-import { LinearProgress, Typography } from '@mui/material';
-import { Box } from '@mui/system';
-import React, { useEffect, useState } from 'react';
-import useAuth from '../../Hooks/useAuth';
+import { LinearProgress, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import useAuth from "../../Hooks/useAuth";
 
 const MyOrder = () => {
-    const { user } = useAuth();
+  const { user } = useAuth();
 
-    const [orders, setOrders] = useState([]);
-    useEffect(() => {
-        fetch(`https://blooming-meadow-89430.herokuapp.com/allOrders/${user.email}`)
-            .then(res => res.json())
-            .then(data => {
-                setOrders(data);
-            })
-    }, []);
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    fetch(`https://blooming-meadow-89430.herokuapp.com/allOrders/${user.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setOrders(data);
+      });
+  }, []);
 
-    const handleDeleteclick = (id) => {
-
-        const proceed = window.confirm('Are you sure, You want to delete?');
-        if (proceed) {
-            const url = `https://blooming-meadow-89430.herokuapp.com/allOrders/${id}`;
-            fetch(url, {
-                method: 'DELETE'
-            })
-                .then(res => res.json())
-                .then(data => {
-                    if (data.deletedCount === 1) {
-                        alert("Successfully deleted ");
-                        const remainingOrders = orders.filter(order => order._id !== id);
-                        setOrders(remainingOrders);
-                    } else {
-                        alert("No documents matched the query. Deleted 0 documents.");
-                    }
-
-                })
-        }
-
-
+  const handleDeleteclick = (id) => {
+    const proceed = window.confirm("Are you sure, You want to delete?");
+    if (proceed) {
+      const url = `https://blooming-meadow-89430.herokuapp.com/allOrders/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount === 1) {
+            alert("Successfully deleted ");
+            const remainingOrders = orders.filter((order) => order._id !== id);
+            setOrders(remainingOrders);
+          } else {
+            alert("No documents matched the query. Deleted 0 documents.");
+          }
+        });
     }
-    return (
-        <div className=" d-flex flex-column align-items-center">
-            <Typography className="service-name" sx={{ textAlign: "center", pt: 3, fontWeight: 800, marginTop: 2, marginBottom: 5, color: "#03e12", fontSize: "50px", fontFamily: "cursive" }} variant="h3">My Orders</Typography>
-            {orders.length === 0 ? <Box sx={{ width: '100%' }}><LinearProgress color="secondary" /></Box>
-                :
-                orders.map(order => <div
-                    key={order._id}
-                    style={{ border: "3px solid", marginTop: "10px", padding: "10px 20px", borderRadius: "25px", backgroundColor: "#FFE4C4" }}
-                >
-                    <span className="fw-bold">Name: </span>{order.name} <span className="fw-bold">Order: </span> {order.orderName} <span className="fw-bold">Status: </span>{order.orderStatus}
-
-                    <button onClick={() => handleDeleteclick(order._id)} className="ms-1 border-0"><i className="fas fa-trash text-danger"></i></button>
-                </div>)
-            }
-
-
-        </div>
-    );
+  };
+  return (
+    <div className="table-responsive">
+      <Typography
+        sx={{
+          textAlign: "center",
+          pt: 3,
+          fontWeight: 800,
+          marginTop: 2,
+          marginBottom: 5,
+          color: "#03e12",
+          fontSize: "50px",
+          fontFamily: "cursive",
+        }}
+        variant="h3"
+      >
+        All Orders
+      </Typography>
+      <table className="table container table-warning  table-hover">
+        <thead className="table-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name:</th>
+            <th scope="col">Booked:</th>
+            <th scope="col">Price:</th>
+            <th scope="col">Phone:</th>
+            <th scope="col">Date:</th>
+            <th scope="col">Status:</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.length === 0 ? (
+            <Box sx={{ width: "100%" }}>
+              <LinearProgress color="secondary" />
+            </Box>
+          ) : (
+            orders.map((order) => (
+              <tr>
+                <th scope="row">*</th>
+                <td>{order.name}</td>
+                <td>{order.orderName}</td>
+                <td>{order.price}$</td>
+                <td>{order.phone}</td>
+                <td>{order?.date?.slice(0, 10)}</td>
+                <td>
+                  {order.orderStatus === "Pending" && (
+                    <i className="me-1 fas fa-spinner text-warning fw-bolder fs-6"></i>
+                  )}
+                  {order.orderStatus === "Shipped" && (
+                    <i className="me-1 fas fa-truck text-info fs-6"></i>
+                  )}
+                  {order.orderStatus === "Delivered" && (
+                    <i class="me-1 fas fa-smile-wink fs-5 text-warning"></i>
+                  )}
+                  {order.orderStatus === "Approved" && (
+                    <i class="me-1 fas fa-check-circle fs-5 text-success"></i>
+                  )}
+                  {order.orderStatus}{" "}
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleDeleteclick(order._id)}
+                    className="ms-1 border-0"
+                  >
+                    <i className="fas fa-trash text-danger"></i>
+                  </button>
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default MyOrder;
